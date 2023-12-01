9e70e031d7a674c157008d5e4ac26662e182ea1f
Aryan Prince <aryanprincesanty@icloud.com>
2023-12-01 23:32:47 +0000

    ✨ Add Inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,16 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], display: "swap" });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />;
+    </div>
+  );
 };
 
 export default api.withTRPC(MyApp);
